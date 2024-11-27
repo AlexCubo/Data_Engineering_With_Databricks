2bded71246f105524b10d67485d31ce68c2f5c48
AlexCubo <AlexCubo>
2024-11-27 14:06:25 +0000

    Executed Exercise DE 4.1.3L
    I jumped directly from bronze level to golden level (i.e. from status_bronze table to email_updates table). I tried to create the silver_table streaming table in the silver level, but the table is inexplicably empty. I did not understand why. So I decided to pass from bronze table directly to the gold table applying all needed transformations. The final table email_updates is exactly as it must be according to the exercise.

--- data-engineering-with-databricks/DE 4 - Delta Live Tables/DE 4.1A - SQL Pipelines/DE 4.1.3L - Status Pipeline Lab.sql
+++ data-engineering-with-databricks/DE 4 - Delta Live Tables/DE 4.1A - SQL Pipelines/DE 4.1.3L - Status Pipeline Lab.sql
@@ -57,20 +57,20 @@
 
 -- COMMAND ----------
 
--- TODO
 CREATE OR REFRESH STREAMING TABLE status_bronze
 AS SELECT current_timestamp() processing_time, input_file_name() source_file, *
 FROM cloud_files("${source}/status", "json");
 
-CREATE OR REFRESH STREAMING LIVE TABLE status_silver
-(CONSTRAINT valid_timestamp EXPECT (status_timestamp > 1640995200) ON VIOLATION DROP ROW)
-AS SELECT * EXCEPT (source_file, _rescued_data)
-FROM LIVE.status_bronze;
+--CREATE OR REFRESH STREAMING TABLE status_silver
+--(CONSTRAINT valid_timestamp EXPECT (status_timestamp > 1640995200) ON VIOLATION DROP ROW)
+--AS SELECT * EXCEPT (source_file, _rescued_data)
+--FROM STREAM(LIVE.status_bronze);
 
 CREATE OR REFRESH LIVE TABLE email_updates
-AS SELECT a.*, b.email
-FROM status_silver a
-INNER JOIN subscribed_order_emails_v b
+(CONSTRAINT valid_timestamp EXPECT (status_timestamp > 1640995200) ON VIOLATION DROP ROW)
+AS SELECT a.* EXCEPT (source_file, _rescued_data), b.email
+FROM LIVE.status_bronze a
+INNER JOIN LIVE.subscribed_order_emails_v b
 ON a.order_id = b.order_id;
 
 -- COMMAND ----------
